29ad9343bd82203751f016242f1a4748ec3602e1
Frappez <sylvain.frappez@gmail.com>
2020-05-09 02:40:54 +0200

    { ADD} Controler IA classico

---
+++ Assets/Scripts/ControlerIA.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ControlerIA : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    NavMeshAgent nMA;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nMA = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nMA.destination = player.transform.position;
+    }
+}
