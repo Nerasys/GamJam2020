b8a53767fee810d3160a3556ddc4e68e2799fa2d
Frappez <sylvain.frappez@gmail.com>
2020-05-09 13:20:29 +0200

    {ADD} Item + Data

---
+++ Assets/Scripts/ItemsRamassable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsRamassable : MonoBehaviour
+{
+    ControlerPlayer cP;
+    DataInfo dI;
+
+
+    public int scoreGive = 10;
+
+
+    void Start()
+    {
+        cP = ControlerPlayer.GetInstance();
+        dI = DataInfo.GetInstance();
+        cP.OnRamassable += UpdateScore;
+        cP.OnRamassable += DestroyGameObject;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void UpdateScore()
+    {
+        dI.score = scoreGive;
+
+    }
+
+    void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+
+
+
+}
