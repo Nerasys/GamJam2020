5e0f47296baac310faaa68874a1f2dc5da516cde
Frappez <sylvain.frappez@gmail.com>
2020-05-10 02:05:12 +0200

    {MODIFY} R

--- Assets/Scripts/Generation.cs
+++ Assets/Scripts/Generation.cs
@@ -125,7 +125,7 @@ public class Generation : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
